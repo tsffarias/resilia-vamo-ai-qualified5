a577a23fc4f714ad1c5af728d5a01a28d0b544c1
Thiago Silva Farias <thiago_protect@hotmail.com>
2021-03-02 21:41:44 -0300

    adicionado podio olimpico versao 2

--- qualified_podio.py
+++ qualified_podio.py
@@ -1,22 +1,22 @@
-def podio_olimpico(tempo_chegada1, tempo_chegada2, tempo_chegada3):
+def podio_olimpico(tempo_chegada1, tempo_chegada2, tempo_chegada3, nome_corredor1, nome_corredor2, nome_corredor3):
 
     resultado = ""
 
     if (tempo_chegada1 < tempo_chegada2 and tempo_chegada1 < tempo_chegada3):
         if (tempo_chegada2 < tempo_chegada3):
-            resultado = f"1 - {tempo_chegada1} minutos\n2 - {tempo_chegada2} minutos\n3 - {tempo_chegada3} minutos\n"
+            resultado = f"1 - {nome_corredor1} - {tempo_chegada1} minutos\n2 - {nome_corredor2} - {tempo_chegada2} minutos\n3 - {nome_corredor3} - {tempo_chegada3} minutos\n"
         else:
-            resultado = f"1 - {tempo_chegada1} minutos\n2 - {tempo_chegada3} minutos\n3 - {tempo_chegada2} minutos\n"
+            resultado = f"1 - {nome_corredor1} - {tempo_chegada1} minutos\n2 - {nome_corredor3} - {tempo_chegada3} minutos\n3 - {nome_corredor2} - {tempo_chegada2} minutos\n"
     elif (tempo_chegada2 < tempo_chegada1 and tempo_chegada2 < tempo_chegada3):
         if (tempo_chegada1 < tempo_chegada3):
-            resultado = f"1 - {tempo_chegada2} minutos\n2 - {tempo_chegada1} minutos\n3 - {tempo_chegada3} minutos\n"
+            resultado = f"1 - {nome_corredor2} - {tempo_chegada2} minutos\n2 - {nome_corredor1} - {tempo_chegada1} minutos\n3 - {nome_corredor3} - {tempo_chegada3} minutos\n"
         else:
-            resultado = f"1 - {tempo_chegada2} minutos\n2 - {tempo_chegada3} minutos\n3 - {tempo_chegada1} minutos\n"
+            resultado = f"1 - {nome_corredor2} - {tempo_chegada2} minutos\n2 - {nome_corredor3} - {tempo_chegada3} minutos\n3 - {nome_corredor1} - {tempo_chegada1} minutos\n"
     else:
         if (tempo_chegada1 < tempo_chegada2):
-            resultado = f"1 - {tempo_chegada3} minutos\n2 - {tempo_chegada1} minutos\n3 - {tempo_chegada2} minutos\n"
+            resultado = f"1 - {nome_corredor3} - {tempo_chegada3} minutos\n2 - {nome_corredor1} - {tempo_chegada1} minutos\n3 - {nome_corredor2} - {tempo_chegada2} minutos\n"
         else:
-            resultado = f"1 - {tempo_chegada3} minutos\n2 - {tempo_chegada2} minutos\n3 - {tempo_chegada1} minutos\n"
+            resultado = f"1 - {nome_corredor3} - {tempo_chegada3} minutos\n2 - {nome_corredor2} - {tempo_chegada2} minutos\n3 - {nome_corredor1} - {tempo_chegada1} minutos\n"
 
     return resultado
 
@@ -24,6 +24,9 @@ def podio_olimpico(tempo_chegada1, tempo_chegada2, tempo_chegada3):
 tempo_chegada1 = 120
 tempo_chegada2 = 90
 tempo_chegada3 = 110
+nome_corredor1 = "Ronaldo"
+nome_corredor2 = "Wanderlei Cordeiro de Lima"
+nome_corredor3 = "Eliud Kipchoge"
 
-resultado = podio_olimpico(tempo_chegada1, tempo_chegada2, tempo_chegada3)
+resultado = podio_olimpico(tempo_chegada1, tempo_chegada2, tempo_chegada3, nome_corredor1, nome_corredor2, nome_corredor3)
 print(resultado)
